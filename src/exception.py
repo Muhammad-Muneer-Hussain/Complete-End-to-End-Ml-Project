ffa1648541dad81b1e68bb491c6c6d6e49a9e44f
Muhammad-Muneer-Hussain <muhammadmuneerhussain85@gmail.com>
2026-04-23 19:43:58 +0500

    Project structure ,logging and exception

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys 
+import logging
+
+def error_message_details(error,error_details:sys):
+    _,_,exe_tb = error_details.exc_info()
+
+    file_name = exe_tb.tb_frame.f_code.co_filename
+    error_message='error occour in python script name[{0}] ,Line No [{1}], error message [{2}]'.format(
+    file_name,exe_tb.tb_lineno,str(error)
+    
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message, error_details:sys):
+        super().__init__(error_message)
+
+        self.error_message = error_message_details(error_message,error_details=error_details)
+
+
+    def __str__(self):
+        return super().__str__()
+    
+if __name__ =="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Divide by Zero')
+        raise CustomException(e,sys)
+    
